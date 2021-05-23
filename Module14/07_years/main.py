d196f2445a71e98150925e2e4d39974201d24b2b
roma-2021 <avr-1234@mail.ru>
2021-05-23 22:15:25 +0300

    finalized task_7 homework_14

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,11 +1,12 @@
 def year_num(year1, year2):
     for year in range(year1, year2 + 1):
-        if len(set(str(year))) == 2 and len(set(str(year)[0:2])) != len(set(str(year)[2:])):
+        num_1 = year // 1000 % 10
+        num_2 = year // 100 % 10
+        num_3 = year // 10 % 10
+        num_4 = year % 10
+        if (num_1 == num_2 == num_3 != num_4) or (num_2 == num_3 == num_4 != num_1) \
+                or (num_1 == num_3 == num_4 != num_2) or (num_1 == num_2 == num_4 != num_3):
             print(year)
-
-
-# TODO, предлагаю пока что решить без len и set, методами из уроков =)
-#  срезы в текущем задании лишние.
 
 year1 = int(input('Ведите первый год: '))
 year2 = int(input('Ведите второй год: '))
