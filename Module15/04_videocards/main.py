2c70c742469e36be74f3b290713ecf39fcebd8ff
roma-2021 <avr-1234@mail.ru>
2021-06-06 22:35:13 +0300

    fixed 4

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,19 +1,15 @@
 count = int(input('Кол-во видеокарт: '))
 graphics_card = []
+max = 0
 for _ in range(1, count + 1):
     number = int(input('Видеокарта: '))
     graphics_card.append(number)
-
+    if number > max:
+        max = number
 new_graphics_card = graphics_card.copy()
-max = graphics_card[0]
-for num in new_graphics_card:
-    if num > max:
-        max = num
-
-# TODO, таким образом, мы удаляем только один элемент списка.
-#  Стоит попробовать определить максимальное значение элемента в цикле с запросом ввода пользователя.
-#  Во втором цикле, стоит ввести проверку и добавить только те элементы, которые меньше максимального. =)
-new_graphics_card.remove(max)
+for i in graphics_card:
+    if i == max:
+        new_graphics_card.remove(max)
 
 print('Старый список видеокарт: ', graphics_card)
 print('Новый список видеокарт: ', new_graphics_card)
