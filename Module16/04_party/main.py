b43c0478df2ed3c99f1f9b3df68c0ce51aff81b7
roma-2021 <avr-1234@mail.ru>
2022-01-11 17:36:43 +0300

    made_1

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,22 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+come = ()
+while come != 'пора спать':
+    number = len(guests)
+    print('Сейчас на вечеринке', number, ' человек:',  guests)
+    come = input('Гость пришел или ушел? ')
 
-# TODO здесь писать код
+    if come == 'пришел' and number <= 5:
+        name = input('Имя гостя: ')
+        guests.append(name)
+        print('Привет', name, '!')
+    elif come == 'ушел' and number > 0:
+        name = input('Имя гостя: ')
+        guests.remove(name)
+    elif come == 'пришел' and number > 5:
+        print('Гостей не может быть больше 6')
+    elif come == 'ушел' and number == 0:
+        print('Гостей нет')
+
+
+
+
