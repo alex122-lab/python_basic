d633025518f444b273b682ba7d7f216fff09fc23
roma-2021 <avr-1234@mail.ru>
2022-01-18 21:59:05 +0300

    made_1

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+count_men = int(input('Кол-во человек: '))
+number = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', number, 'человек')
+list_men = list(range(1, count_men + 1))
+out = 0
+for _ in range(count_men - 1):
+    print('Текущий круг людей:', list_men)
+    start_count = out % len(list_men)
+    out = (start_count + number - 1) % len(list_men)
+    print('Начало счёта с номера', list_men[start_count])
+    print('Выбывает человек под номером', list_men[out])
+    list_men.remove(list_men[out])
+
+print('Остался человек под номером', list_men)
