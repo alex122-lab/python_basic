4fd2b8bb5a630918e5a4778bacd5b8aee477b264
roma-2021 <avr-1234@mail.ru>
2022-01-18 11:22:49 +0300

    made_1

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+list_skates = []
+list_men = []
+count = 0
+count_skates = int(input('Кол-во коньков: '))
+for i in range(count_skates):
+    print('Размер', i + 1, 'пары: ')
+    size = int(input())
+    list_skates.append(size)
+list_skates = sorted(list_skates)
+count_men = int(input('Кол-во людей: '))
+for i in range(count_men):
+    print('Размер ноги', i + 1, 'человека: ')
+    foot = int(input())
+    list_men.append(foot)
+list_men = sorted(list_men)
+for foot in list_men:
+    for skate in list_skates:
+        if foot <= skate:
+            list_men.remove(foot)
+            list_skates.remove(skate)
+            count += 1
+print('Наибольшее кол-во людей, которые могут взять ролики:', count)
