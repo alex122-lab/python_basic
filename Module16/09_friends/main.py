4a1ed751a89ca323c07bb6871c4eabd5e65cbcac
roma-2021 <avr-1234@mail.ru>
2022-01-19 11:22:37 +0300

    made_1

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+N = int(input('Кол-во друзей: '))
+N_kredit = int(input('Долговых расписок: '))
+list_friends = []
+number = 0
+
+for i in range(N):
+    list_friends.append(list(range(2)))
+    list_friends[i][0] = i + 1
+    list_friends[i][1] = 0
+
+for i in range(N_kredit):
+    number += 1
+    print(number,'расписка')
+    to_who = int(input('Кому: '))
+    from_who = int(input('От кого: '))
+    if to_who != from_who:
+        how_much = int(input('Сколько: '))
+        list_friends[to_who - 1][1] -= how_much
+        list_friends[from_who - 1][1] += how_much
+    else:
+        print('Самому себе нельзя давать в долг')
+
+print('\nБаланс друзей:')
+for i in range(N):
+    print(list_friends[i][0], ':',list_friends[i][1] )
