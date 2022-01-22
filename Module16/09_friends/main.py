511cfa4f1f20f78d50227783ad7a415c68faed54
roma-2021 <avr-1234@mail.ru>
2022-01-22 12:13:05 +0300

    made_2

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -3,12 +3,8 @@ N_kredit = int(input('Долговых расписок: '))
 list_friends = []
 number = 0
 
-# TODO, предлагаю упростить реализацию. Создать список [0, 0, 0], в котором элемент — это сумма долга, а его позиция, это номер друга.
-#  И работать только с этим списком.
 for i in range(N):
-    list_friends.append(list(range(2)))
-    list_friends[i][0] = i + 1
-    list_friends[i][1] = 0
+    list_friends.append(0)
 
 for i in range(N_kredit):
     number += 1
@@ -17,11 +13,12 @@ for i in range(N_kredit):
     from_who = int(input('От кого: '))
     if to_who != from_who:
         how_much = int(input('Сколько: '))
-        list_friends[to_who - 1][1] -= how_much
-        list_friends[from_who - 1][1] += how_much
+        list_friends[to_who - 1] -= how_much
+        list_friends[from_who - 1] += how_much
+        print(list_friends)
     else:
         print('Самому себе нельзя давать в долг')
 
 print('\nБаланс друзей:')
 for i in range(N):
-    print(list_friends[i][0], ':',list_friends[i][1] )
+    print(i + 1, ':',list_friends[i])
