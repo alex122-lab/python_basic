3e90412b44d93fedf965a651e7979bc0dd4b61ef
roma-2021 <avr-1234@mail.ru>
2022-01-24 20:28:08 +0300

    made_1

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+first_index = text.index('h')
+reverse_text = text[::-1]
+end_index = len(text) - reverse_text.index('h') - 1
+result = text[end_index - 1:first_index:-1]
+print('Развернутая последовательность между первым и последним h: ', result)
