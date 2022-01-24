cc9d2eb716a595f138ff3c72786a5c1e4585eb5b
roma-2021 <avr-1234@mail.ru>
2022-01-24 16:28:31 +0300

    made_1

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+word_vowels = ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+list_sentence = input('Введите текст: ')
+list_vowels = [letter for letter in list_sentence if letter in word_vowels]
+print('\nСписок гласных букв', list_vowels)
+print('Длина списка', len(list_vowels))
