39021af142f20a30b15b2f506e337b8ceddbddf4
roma-2021 <avr-1234@mail.ru>
2022-01-24 21:43:23 +0300

    made_1

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import random
+N = int(input('Кол-во чисел в списке: '))
+list_start = [random.randint(0, 2) for _ in range(N)]
+print('Список до сжатия:', list_start[:])
+for i in range(list_start.count(0)):
+    list_start.remove(0)
+    list_start.append(0)
+list_end = [list_start[num] for num in range(len(list_start)) if list_start[num] != 0]
+print('Список после сжатия:', list_end)
+
