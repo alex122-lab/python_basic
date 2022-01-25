b8cfa8fc7d29f71bbc9089a9003db1d7de2ec6ac
roma-2021 <avr-1234@mail.ru>
2022-01-25 15:29:14 +0300

    made_1

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import random
+sticks = int(input('Кол-во палок: '))
+thows = int(input('Кол-во бросков: '))
+list_stick = [1 for _ in range(sticks)]
+
+for _ in range(thows):
+    list_throw = [random.randint(0, 1) if list_stick[num] == 1 else 0 for num in range(sticks)]
+    list_stick = [list_stick[num] if list_throw[num] == 0 else 0 for num in range(sticks)]
+list_end = ['I' if list_stick[num] == 1 else '.' for num in range(sticks)]
+
+print('Результат: ', *list_end, sep = "")
+
