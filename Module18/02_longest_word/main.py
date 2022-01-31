86f0e0b66804a2c03fde4763c1af1a23609c4944
roma-2021 <avr-1234@mail.ru>
2022-01-31 21:45:35 +0300

    made_1

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+text = input('Введите строку: ').split()
+text.sort(key=len, reverse=True)
+max_word = text[0]
+max_len_word = len(max_word)
+print('Самое длинное слово:', max_word)
+print('Длина этого слова:', max_len_word)
+
+# 2 решение
+# text = input('Введите строку: ').split()
+# list_len_word = [len(word) for word in text]
+# max_len_word = max(list_len_word)
+# index_max_word = list_len_word.index(max_len_word)
+# max_word = text[index_max_word]
+# print('Самое длинное слово:', max_word)
+# print('Длина этого слова:', max_len_word)
+
