574bf091e9af4497bff64803aeaeac4a7c0246f7
roma-2021 <avr-1234@mail.ru>
2022-02-02 21:42:15 +0300

    made_1

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+file_name = input('Название файла: ')
+simbols = ('@', '№', '$', '%', '^', '&', '*', '(', ')')
+flag = True
+if file_name.startswith(simbols):
+    print('Ошибка: название начинается на один из специальных символов')
+    flag = False
+if not file_name.endswith(('.txt', '.docx')):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+    flag = False
+
+if flag:
+    print('Файл назван верно.')
