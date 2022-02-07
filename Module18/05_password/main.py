2e0d0788f3dc1221e42c440d3a2bd4c0039e386b
roma-2021 <avr-1234@mail.ru>
2022-02-07 17:52:10 +0300

    made_1

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+flag = True
+while True:
+    password = input('Придумайте пароль: ')
+    len_p = len(password)
+    count_up_letter = 0
+    count_d = 0
+    for letter in password:
+        if letter not in alphabet:
+            flag = False
+        elif letter.isupper():
+            count_up_letter += 1
+        elif letter.isdigit():
+            count_d += 1
+    if flag and len_p >= 8 and count_up_letter >= 1 and count_d >= 3:
+        print('Файл назван верно.')
+        break
+    else:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+
