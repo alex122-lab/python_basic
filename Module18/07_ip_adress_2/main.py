cc63e2cf4e60ee98ee657d0bf97de908fbc7f5ec
roma-2021 <avr-1234@mail.ru>
2022-02-08 12:26:52 +0300

    made_1

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+ip_ad = input('Введите IP: ').split('.')
+if len(ip_ad) != 4:
+    print('Адрес - это четыре числа, разделенные точками')
+else:
+    for num in ip_ad:
+        if not num.isnumeric():
+            print(num, '- не целое число')
+        elif int(num) >= 255:
+            print(num, 'превышает 255')
