0e72522da3345b6bee74351950e2467e28c743d4
roma-2021 <avr-1234@mail.ru>
2022-02-08 08:04:20 +0300

    made_1

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+cnt = 0
+code_text = ''
+for i in range(len(text)):
+    sym = text[i]
+    cnt += 1
+    if i == len(text) - 1:
+        code_text = code_text + text[i] + str(cnt)
+        break
+    if text[i] != text[i + 1]:
+        code_text = code_text + sym + str(cnt)
+        cnt = 0
+print('Закодированная строка:', code_text)
