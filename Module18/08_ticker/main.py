5735ecf69ef5412cc8cdeb847b8412e7e3dc29b9
roma-2021 <avr-1234@mail.ru>
2022-02-08 16:30:51 +0300

    made_1

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+text_1 = input('Первая строка: ')
+text_2 = input('Вторая строка: ')
+flag = False
+step = -1
+for _ in range(len(text_1)):
+    step += 1
+    text_step = text_2[-step:] + text_2[:-step]
+    if text_step == text_1:
+        flag = True
+        break
+if flag:
+    print('Первая строка получается из второй со сдвигом', step, '.')
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
