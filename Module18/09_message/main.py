8af280e51d7d6de85104f7c17ecf048ce4abec43
roma-2021 <avr-1234@mail.ru>
2022-02-11 23:12:13 +0300

    made_1

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+text = input('Сообщение: ')
+r_alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+new_text = []
+start = 0
+for i, sym in enumerate(text):
+    if sym not in r_alphabet:
+        new_text.insert(i, sym)
+        start = i + 1
+    else:
+        new_text.insert(start, sym)
+
+print('Новое сообщение:', ''.join(new_text))
