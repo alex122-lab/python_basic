b7ec3cd8cd4cb0091420186ebe41f8bb7211f235
roma-2021 <avr-1234@mail.ru>
2022-02-14 20:29:25 +0300

    made_2

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,16 +1,9 @@
 file_name = input('Название файла: ')
 simbols = ('@', '№', '$', '%', '^', '&', '*', '(', ')')
-flag = True
-if file_name.startswith(simbols):
-    print('Ошибка: название начинается на один из специальных символов')
-    flag = False
-if not file_name.endswith(('.txt', '.docx')):
-    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
-    flag = False
-# TODO, Предлагаю убрать из решения переменную флаг и добавить блок else в цикл for.
-#  Если из цикла при помощи break не вышли, отработает блок else цикла for.
-#  Немного подробней, про блоки else циклов for и while, можно почитать тут
-#  https://habr.com/ru/post/148365/
-
-if flag:
-    print('Файл назван верно.')
+for _ in range(1):
+    if file_name.startswith(simbols):
+        print('Ошибка: название начинается на один из специальных символов')
+    if not file_name.endswith(('.txt', '.docx')):
+        print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+    else:
+        print('Файл назван верно.')
