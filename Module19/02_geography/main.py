e0d2660a24740d84f06c11f8296de32860337627
roma-2021 <avr-1234@mail.ru>
2022-02-18 22:09:06 +0300

    made_1

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+city_country = dict()
+count_country = int(input('Кол-во стран: '))
+for i in range(1, count_country + 1):
+    print(i, 'страна: ')
+    country_city_title = input().split()
+    city_country.update({country_city_title[0]:country_city_title[1:]})
+
+for i in range(1, 4):
+    title = input(f'{i} город: ')
+    for country in city_country:
+        for city in city_country[country]:
+            if title == city:
+                print('Город', city, 'расположен в стране', country, '.')
+else:
+    print('По городу', title, 'данных нет.')
+
