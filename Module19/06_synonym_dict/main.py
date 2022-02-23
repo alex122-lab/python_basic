b0207b0e6e7607d1583a2d766e20e7eea4a3f1ef
roma-2021 <avr-1234@mail.ru>
2022-02-23 21:24:16 +0300

    made_1

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+orders = dict()
+count = int(input('Введите кол-во заказов: '))
+for i in range(1, count + 1):
+    two_words = input(f'{i} пара: ').split(' - ')
+    words.update({two_words[0]:two_words[1]})
+
+new_word = input('Введите слово: ').lower()
+for word, synonym in words.items():
+    if new_word == word.lower():
+        print('Синоним:', synonym)
+        break
+    elif new_word == synonym.lower():
+        print('Синоним:', word)
+        break
+else:
+    print('Такого слова в словаре нет.')
