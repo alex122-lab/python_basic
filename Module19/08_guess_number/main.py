69dcfb6b3aaba51665fe38664ad4f04762fcb3c8
roma-2021 <avr-1234@mail.ru>
2022-03-27 18:59:44 +0300

    made_1

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+number_max = int(input('Введите максимальное число: '))
+numbers_dict = {str(num_dict): 'Нет' for num_dict in range(1, number_max + 1)}
+
+while True:
+    boris_phrase = input('Нужное число есть среди вот этих чисел: ')
+    if boris_phrase == 'Помогите!':
+        print('Артём мог загадать следующие числа: ', end='')
+        for number, word in numbers_dict.items():
+            if word == 'Да':
+                print(number, end=' ')
+        break
+    else:
+        boris_phrase = boris_phrase.split()
+        artems_answer = input('Ответ Артёма: ')
+        for num in numbers_dict:
+            if num in boris_phrase:
+                numbers_dict[num] = artems_answer
--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,48 @@
 # TODO здесь писать код
+def height(man):
+    if man not in p_tree:
+        return 0
+    else:
+        return 1 + height(p_tree[man])
+
+
+p_tree = {}
+n = int(input())
+for i in range(n - 1):
+    child, parent = input().split()
+    p_tree[child] = parent
+
+heights = {}
+for man in set(p_tree.keys()).union(set(p_tree.values())):
+    heights[man] = height(man)
+
+for key, value in sorted(heights.items()):
+    print(key, value)
+
+
+d = {}
+for i in range(int(input('Введите кол-во человек: '))):
+    name_baby, name_father = input(f'{i + 1} пара: ').rsplit(maxsplit=2)
+    dct = d.get(name_father, {})
+    print(dct)
+    dct[name_father] = name_baby
+    print(dct)
+
+   d[name_father] =  dct
+    print(d)
+    dct = {}
+for name in sorted(d):
+    print(f'{name}:')
+    for pizza, amount in sorted(d.get(name).items()):
+        print(f'{pizza}: {amount}')
+    print()
+
+# Введите количество человек: 9
+# 1 пара: Alexei Peter_I
+# 2 пара: Anna Peter_I
+# 3 пара: Elizabeth Peter_I
+# 4 пара: Peter_II Alexei
+# 5 пара: Peter_III Anna
+# 6 пара: Paul_I Peter_III
+# 7 пара: Alexander_I Paul_I
+# 8 пара: Nicholaus_I Paul_I
