23ebba165047535d8a42afcbe069644a1f5cffab
roma-2021 <avr-1234@mail.ru>
2022-04-02 08:38:40 +0300

    made_2

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -4,14 +4,13 @@ for i in range(1, count_country + 1):
     print(i, 'страна: ')
     country_city_title = input().split()
     city_country.update({country_city_title[0]:country_city_title[1:]})
-
+print(city_country)
 for i in range(1, 4):
-    title = input(f'{i} город: ')
+    city = input(f'{i} город: ')
     for country in city_country:
-        # TODO, цикл for ниже получился лишним. Наличие ключа в словаре можно проверить при помощи оператора вхождения "in".
-        for city in city_country[country]:
-            if title == city:
-                print('Город', city, 'расположен в стране', country, '.')
-else:  # TODO, блок else будет выполнен всегда, т.к. в цикле for отсутствует break.
-    print('По городу', title, 'данных нет.')
+        if city in city_country[country]:
+            print('Город', city, 'расположен в стране', country, '.')
+            break
+    else:
+        print('По городу', city, 'данных нет.')
 
