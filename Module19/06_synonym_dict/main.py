783ee73f886699b0d767ea6e1224f32c202683e9
roma-2021 <avr-1234@mail.ru>
2022-04-02 09:05:33 +0300

    made_2

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,23 +1,14 @@
 orders = dict()
-count = int(input('Введите кол-во заказов: '))
+count = int(input('Введите количество пар слов: '))
+words = {}
 for i in range(1, count + 1):
-    two_words = input(f'{i} пара: ').split(' - ')
-    words.update({two_words[0]:two_words[1]})
-    # TODO  предлагаю упростить реализацию, и при запросе ввода пользователя добавлять в  словарь сразу два ключа
-    #  Привет: Здравствуйте
-    #  Здравствуйте: Привет
-    #  Вложенный цикл for, в таком случае, будет лишним.
+    two_words = input(f'{i} пара: ').lower().split(' - ')
+    words.update({two_words[0]:two_words[1], two_words[1]:two_words[0]})
 
-new_word = input('Введите слово: ').lower()
-for word, synonym in words.items():
-    if new_word == word.lower():
-        print('Синоним:', synonym)
-        break
-    elif new_word == synonym.lower():
-        print('Синоним:', word)
-        break
-else:
-    print('Такого слова в словаре нет.')
-
-
-# TODO, пока что, код отрабатывается с ошибкой, т.к. переменной words не существует.
+new_word = ''
+while new_word not in words:
+    new_word = input('Введите слово: ').lower()
+    if new_word in words:
+        print('Синоним:', words[new_word])
+    else:
+        print('Такого слова в словаре нет.')
