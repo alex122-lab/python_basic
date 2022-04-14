7ed1c5f27be108a300e63fed2e839c69138467dc
roma-2021 <avr-1234@mail.ru>
2022-04-14 22:12:33 +0300

    made_1

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def is_prime(n):
+    if n < 2 or n % 2 == 0:
+        return n == 2
+    else:
+        d = 3
+        while n % d != 0:
+            d += 1
+        return d == n
+
+def crypto(symbols):
+    prime_indexes = []
+    for i, sym in enumerate(symbols):
+        if is_prime(i):
+            prime_indexes.append(sym)
+    return prime_indexes
+
+def crypto_return(symbols):
+    return [sym for i, sym in enumerate(symbols) if is_prime(i)]
+
+print(crypto([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto('О Дивный Новый мир!'))
+
+print(crypto_return([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto_return('О Дивный Новый мир!'))
+
+
