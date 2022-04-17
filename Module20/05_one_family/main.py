15453dad1e58645b40073ac5c414ccccde675f87
roma-2021 <avr-1234@mail.ru>
2022-04-17 22:38:01 +0300

    made_1

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+dict_famaly = {
+    ("Сидоров", "Никита"): 35,
+    ("Сидорова", "Анна"): 34,
+    ("Сидоров", "Павел"): 10
+}
+famaly = input('Введите фамилию: ').lower()
+print()
+for fam, age in dict_famaly.items():
+    men = fam[0].lower()
+    if (famaly == men) or (famaly + 'а' == men) or (famaly == men + 'а'):
+        print(fam[0], fam[1], age)
+
