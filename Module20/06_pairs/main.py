7a683fbf5f197090b9298780c5166ae6d397c332
roma-2021 <avr-1234@mail.ru>
2022-04-18 20:50:41 +0300

    made_1

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+original_list = [num for num in range(10)]
+new_list = []
+# 1) способ
+for i in range(0, len(original_list), 2):
+    pair_num = i, i + 1
+    new_list.append(pair_num)
+
+print(new_list)
+# 2) способ
+new_list = [(i, i + 1) for i in range(0, len(original_list), 2)]
+print(new_list)
