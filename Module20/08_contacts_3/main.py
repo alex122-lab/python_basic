0c45a9a67f952383cbac0d77c9b6a597aa50c55b
roma-2021 <avr-1234@mail.ru>
2022-04-21 08:23:41 +0300

    made_1

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+dict_tel = {}
+while True:
+    action_num = int(input('Введите номер действия:\n 1. Добавить контакт \n 2. Найти человека \n'))
+    if action_num == 1:
+        name_surname = tuple(input('Введите имя и фамилию нового контакта(через пробел): ').split())
+        for name_sur in dict_tel:
+            if name_sur == name_surname:
+                print('Такой человек уже есть в контактах.')
+                print('Текущий словарь контактов:', dict_tel)
+                break
+        else:
+            num_tel = int(input('Введите номер телефона: '))
+            dict_tel.update({name_surname : num_tel})
+            print('Текущий словарь контактов:', dict_tel)
+    elif action_num == 2:
+        surname = input('Введите фамилию для поиска: ')
+        for fam, age in dict_tel.items():
+            if fam[1].lower() == surname.lower():
+                print(fam[0], fam[1], age)
+    else:
+        print('Введен неправильный номер действия!')
+
