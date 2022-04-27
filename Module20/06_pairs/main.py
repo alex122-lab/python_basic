3c675839629b63546d315002bab9e88d2122208e
roma-2021 <avr-1234@mail.ru>
2022-04-27 11:04:01 +0300

    made_1

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -9,3 +9,8 @@ print(new_list)
 # 2) способ
 new_list = [(i, i + 1) for i in range(0, len(original_list), 2)]
 print(new_list)
+# 3 способ
+new_list_0 = [(i) for i in range(0, len(original_list), 2)]
+new_list_1 = [(i + 1) for i in range(0, len(original_list), 2)]
+new_list_pair = zip(new_list_0, new_list_1)
+print(list(new_list_pair))
