591963313edad2b53106cd60189c55928b65a13a
roma-2021 <avr-1234@mail.ru>
2022-05-04 19:59:15 +0300

    made_1

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def fib(num_pos):
+    if num_pos == 1 or num_pos == 2:
+        return 1
+    else:
+        sum_num = fib(num_pos - 1) + fib(num_pos - 2)
+        return sum_num
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+print(fib(num_pos))
