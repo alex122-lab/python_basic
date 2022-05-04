83e3380d288e64c3c9f6d31629c4f548d83407fc
roma-2021 <avr-1234@mail.ru>
2022-05-04 19:43:11 +0300

    made_1

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def numbers(number, count):
+    if count < number:
+        count += 1
+        print(count)
+        return numbers(number, count)
+
+count = 0
+num = int(input('Введите num: '))
+numbers(num, count)
