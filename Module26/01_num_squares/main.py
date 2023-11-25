31a419d66ea394c724a74cbcc592645886736ed1
roma-2021 <avr-1234@mail.ru>
2023-11-25 21:31:26 +0300

    Made

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+class Iterator:
+    """Итератор последовательности из N элементов"""
+    def __init__(self, num):
+        self.__num = num
+        self.__counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.__counter < self.__num:
+            self.__counter += 1
+            return self.__counter ** 2
+        else:
+            raise StopIteration
+
+def square_gen(num):
+    """Генератор последовательности из N чисел"""
+    for elem in range(1,num + 1):
+        yield elem ** 2
+
+square_gen_exp = (num ** 2 for num in range(1,11))
+"""Генераторное выражение"""
+
+my_iter = Iterator(num=10)
+for elem in my_iter:
+    print(elem, end=' ')
+print('\n')
+
+for elem in square_gen(num=10):
+    print(elem, end=" ")
+print('\n')
+
+for elem in square_gen_exp:
+    print(elem, end=" ")
--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import os
+
+folder = os.path.abspath(os.path.join('..'))
+
+def gen_files_path(directory):
+    for links, dirs, files in list(os.walk(directory)):
+        for file in files:
+            yield links + '/' + file
+
+for file in gen_files_path(folder):
+   print(file)
